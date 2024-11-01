132310d1cf1bdb4843e80bb6f296838a8a5a2bd9
Küng Hansueli <system-shock@web.de>
2024-11-01 14:26:14 +0100

    Initial Commit

---
+++ You are Awesome!/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  You are Awesome!
+//
+//  Created by Küng Hansueli on 01.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ You are Awesome!Tests/You_are_Awesome_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  You_are_Awesome_Tests.swift
+//  You are Awesome!Tests
+//
+//  Created by Küng Hansueli on 01.11.2024.
+//
+
+import Testing
+@testable import You_are_Awesome_
+
+struct You_are_Awesome_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
