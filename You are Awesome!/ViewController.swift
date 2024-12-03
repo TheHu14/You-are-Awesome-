c4d0dd171533a8c20e34bbfc8f05e696f2abb81d
Küng Hansueli <system-shock@web.de>
2024-12-03 09:18:01 +0100

    2.6 Completed

--- You are Awesome!/ViewController.swift
+++ You are Awesome!/ViewController.swift
@@ -8,23 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var massageLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         print("👍🏼 viewDidLoad has run👍🏼!")
         massageLabel.text = "Fabulous? That's You!"
+        imageView.image = UIImage(named: "image0")
     }
 
     @IBAction func messageButtonPressed(_ sender: UIButton) {
         print("😎 The message button was pressed!")
         massageLabel.text = "You are awesome!"
+        imageView.image = UIImage(named: "image1")
     
     }
     
     @IBAction func massageOtherButtonPressed(_ sender: Any) {
         print("The other message button was pressed!")
         massageLabel.text = "You are Great!🙌🏼"
+        imageView.image = UIImage(named: "image2")
     }
 }
 
